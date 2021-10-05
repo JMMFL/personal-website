35ec827b0eb028382f35764b5677d19c26295f07
JMMFL <mehravarj+github@gmail.com>
2021-10-05 18:38:56 -0400

    Created Navigation component.

---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaGithub, FaFilePdf, FaLinkedin } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+import { Menu, NavMenu, IconMenu, Emblem, EmblemText, EmblemIcon } from "./styled/Library";
+
+function Navigation() {
+    return (
+        <Menu>
+            <NavMenu>
+                <li><a href="#works">Works</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="mailto:mehravarj+github@gmail.com">Contact</a></li>
+            </NavMenu>
+            <Emblem>
+                <EmblemIcon />
+                <EmblemText>Jordan<br></br>Mehravar</EmblemText>
+            </Emblem>
+            <div style={{width: "296px", display: "flex", justifyContent: "end"}}>
+                <IconMenu>
+                    <li><FaFilePdf size={26} /></li>
+                    <li><a href="https://github.com/JMMFL" target="_blank" rel="noreferrer"><FaGithub size={26} /></a></li>
+                    <li><FaLinkedin size={26} /></li>
+                    <li><a href="mailto:mehravarj+github@gmail.com"><MdAlternateEmail size={26} /></a></li>
+                </IconMenu>
+            </div>
+        </Menu>
+    );
+}
+
+export default Navigation;
