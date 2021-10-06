1502104c7f88bb48ae2b6a279be1c8b8f2d2b35b
JMMFL <mehravarj+github@gmail.com>
2021-10-06 18:01:14 -0400

    Created Works component.

---
+++ src/components/Works.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Carousel, ProjectDescription, ProjectInfo, List, Description, DescriptionTitle, ProjectTopics, Input, Label, ProjectPreview, DemoButton, MoreButton, ProjectNav, Arrow, NavArrows } from "./styled/Library";
+import { BsLightningFill } from "react-icons/bs";
+import { MdOutlineArrowForwardIos, MdOutlineArrowBackIos } from "react-icons/md";
+import { GoCode } from "react-icons/go";
+
+function Works({projects}) {
+    const [index, setIndex] = useState(0);
+    const project = projects[index];
+    const maxIndex = projects.length - 1;
+    const canGoNext = index < maxIndex;
+    const canGoBack = index > 0;
+    
+
+    return (
+        <Carousel>
+            <ProjectInfo>
+                <ProjectNav>
+                    <List>
+                        {projects.map(({name}, i) => {
+                            let props = {}
+                            props.checked = i === index ? "checked" : "";
+                
+                            return (
+                                <div style={{marginBottom: 10}} key={i}>
+                                    <Input
+                                        style={{display: "none"}} 
+                                        type="radio" 
+                                        id={name} 
+                                        name="project" 
+                                        value={name} 
+                                        onClick={() => setIndex(i)}
+                                        {...props}
+                                    />
+                                    <Label htmlFor={name}>{name}</Label>
+                                </div>
+                            )
+                        })}
+                    </List>
+                    <NavArrows>
+                        <Arrow style={{opacity: `${index === 0 ? 0.2 : 1}`}} onClick={() => setIndex(canGoBack ? index - 1 : index )}><MdOutlineArrowBackIos size={20} /></Arrow>
+                        <Arrow style={{opacity: `${index === maxIndex ? 0.2 : 1}`}} onClick={() => setIndex(canGoNext ? index + 1 : index)}><MdOutlineArrowForwardIos size={20}/></Arrow>
+                    </NavArrows>
+                </ProjectNav>
+                <ProjectDescription>
+                        <DescriptionTitle>Description</DescriptionTitle>
+                        <Description>{project.description}</Description>
+                    </ProjectDescription>
+                <ProjectTopics>{project.topics}</ProjectTopics>
+            </ProjectInfo>
+            <ProjectPreview>
+                <div style={{alignSelf: "start"}}>
+                    <DemoButton href={project.link} target="_blank" rel="noreferrer">Demo<BsLightningFill size={15} style={{position: "relative", top: "3px", left: "3px"}} /></DemoButton>
+                    <MoreButton href={project.github} target="_blank" rel="noreferrer">More<GoCode size={15} style={{position: "relative", top: "2px", left: "4px"}} /></MoreButton>
+                </div>
+            </ProjectPreview>
+        </Carousel>
+    )
+}
+
+export default Works;
--- src/components/styled/Library.js
+++ src/components/styled/Library.js
@@ -57,3 +57,114 @@ export const HeaderText = styled.h1`
     font-weight: normal;
     margin-bottom: 197px;
 `
+
+export const Carousel = styled.div`
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    height: 823px;
+    width: 1589px;
+    border-radius: 35px;
+    background-color: var(--carousel-color);
+    padding: 40px;
+    margin-bottom: 174px;
+`
+export const ProjectInfo = styled.div`
+    align-self: center;
+    width: 337px;
+`
+
+export const ProjectNav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    height: 208px;
+    border-bottom: 1px solid var(--border-color);
+    margin-bottom: 31px;
+`
+
+export const List = styled.ul`
+    text-transform: uppercase;
+    font-size: 26px;
+    font-weight: bold;
+`   
+
+export const Input = styled.input`
+    display: none;
+    
+    :checked + label {
+        opacity: 1;
+    }
+`
+
+export const Label = styled.label`
+    opacity: 0.1;
+`
+
+export const ProjectDescription = styled.div`
+    height: 419px;
+    border-bottom: 1px solid var(--border-color);
+    margin-bottom: 34px;
+`
+
+export const DescriptionTitle = styled.h1`
+    font-size: 14px;
+    text-transform: uppercase;
+    font-weight: bold;
+    margin-bottom: 23px;
+`
+
+export const Description = styled.p`
+    font-size: var(--description-text);
+    line-height: var(--description-height);
+    font-weight: lighter;
+`
+
+export const ProjectTopics = styled.p`
+    font-size: 13px;
+`
+
+export const ProjectPreview = styled.div`
+    padding-right: 35px;
+    padding-top: 30px;
+    display: flex;
+    justify-content: end;
+    align-self: center;
+    width: 1102px;
+    height: 737px;
+    background: green;
+    border-radius: 16px;
+`
+
+export const Button = styled.a`
+    display: block;
+    width: 100px;
+    height: 40px;
+    border-radius: 30px;
+    text-align: center;
+    font-size: 14px;
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 10px;
+`
+
+export const DemoButton = styled(Button)`
+    background: white;
+    color: black;
+    margin-bottom: 10px;
+`
+
+export const MoreButton = styled(Button)`
+    background: transparent;
+    border: 1px solid white;
+    color: white;
+    border-radius: 30px;
+`
+
+export const NavArrows = styled.div`
+`
+
+export const Arrow = styled.button`
+    background: transparent;
+    border: none;
+    display: inline;
+`
