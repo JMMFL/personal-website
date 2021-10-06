378e9079c444c43a2f7d02f033032a6fb3a0f4c5
JMMFL <mehravarj+github@gmail.com>
2021-10-06 11:30:29 -0400

    Added data for projects.

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+const data = [
+    {name: "checkm8", link: "https://checkm8.me/", gif: null},
+    {name: "super mario cape", link: "https://supermariocape.xyz/", gif: null},
+    {name: "matt mazzocchi", link: "https://mazzmatt.com/", gif: null},
+    {name: "old galt bottle", link: "https://ogbottleshop.ca/", gif: null}
+]
+
+export default data;
