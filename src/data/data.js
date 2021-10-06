0ddbd9b662954b1b4e98ba19d1912e04b0ccc876
JMMFL <mehravarj+github@gmail.com>
2021-10-06 12:09:45 -0400

    Added description and github property.

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,36 @@
 const data = [
-    {name: "checkm8", link: "https://checkm8.me/", gif: null},
-    {name: "super mario cape", link: "https://supermariocape.xyz/", gif: null},
-    {name: "matt mazzocchi", link: "https://mazzmatt.com/", gif: null},
-    {name: "old galt bottle", link: "https://ogbottleshop.ca/", gif: null}
+    {
+        name: "checkm8", 
+        link: "https://checkm8.me/", 
+        gif: null,
+        description: "Web app for checking Chess.com player stats and match history.",
+        github: "https://github.com/JMMFL/checkm8-app"
+    },
+
+    {
+        name: "super mario cape", 
+        link: "https://supermariocape.xyz/", 
+        gif: null,
+        description: "Browser game inspired by Super Mario World and Flappy Bird.",
+        github: "https://github.com/JMMFL/super-mario-cape"
+    },
+
+    {
+        name: "matt mazzocchi", 
+        link: "https://mazzmatt.com/", 
+        gif: null,
+        description: "Reponsive portfolio for Canadian photographer Matt Mazzocchi.",
+        github: "https://github.com/JMMFL/photography-portfolio"
+    
+    },
+
+    {
+        name: "old galt bottle", 
+        link: "https://ogbottleshop.ca/", 
+        gif: null,
+        description: "Shopify store for specialty beer boutique based in Cambridge.",
+        github: null
+    }
 ]
 
 export default data;
