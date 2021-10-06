bdc0b60c5039678868263dc90c7a374babab5a21
JMMFL <mehravarj+github@gmail.com>
2021-10-06 14:31:08 -0400

    Added works section.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import Works from "./components/Works";
+import data from "./data/data";
 
 function App() {
   return (
     <>
       <Navigation />
       <Header />
+      <Works projects={data} />
     </>
   );
 }
