4e3caf7819e1d344e10b0f18cf1dd5c0d0a2a182
JMMFL <mehravarj+github@gmail.com>
2021-10-06 12:25:52 -0400

    Added topics property.

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,8 @@ const data = [
         link: "https://checkm8.me/", 
         gif: null,
         description: "Web app for checking Chess.com player stats and match history.",
-        github: "https://github.com/JMMFL/checkm8-app"
+        github: "https://github.com/JMMFL/checkm8-app",
+        topics: "React - Styled Components - API Requests"
     },
 
     {
@@ -12,7 +13,8 @@ const data = [
         link: "https://supermariocape.xyz/", 
         gif: null,
         description: "Browser game inspired by Super Mario World and Flappy Bird.",
-        github: "https://github.com/JMMFL/super-mario-cape"
+        github: "https://github.com/JMMFL/super-mario-cape",
+        topics: "ES6+ - DOM - OOP Programming - Canvas"
     },
 
     {
@@ -20,8 +22,8 @@ const data = [
         link: "https://mazzmatt.com/", 
         gif: null,
         description: "Reponsive portfolio for Canadian photographer Matt Mazzocchi.",
-        github: "https://github.com/JMMFL/photography-portfolio"
-    
+        github: "https://github.com/JMMFL/photography-portfolio",
+        topics: "HTML - Flex Box - Sass - Reponsive Design"
     },
 
     {
@@ -29,7 +31,8 @@ const data = [
         link: "https://ogbottleshop.ca/", 
         gif: null,
         description: "Shopify store for specialty beer boutique based in Cambridge.",
-        github: null
+        github: null,
+        topics: "Liquid - CSS - Custom Cart - Custom Theme"
     }
 ]
 
