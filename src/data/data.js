734cc9e8fa59d895deaaa60ef2adbe143a98efdc
JMMFL <mehravarj+github@gmail.com>
2021-10-06 18:00:49 -0400

    Adjusted descripition for better spacing.

--- src/data/data.js
+++ src/data/data.js
@@ -5,34 +5,34 @@ const data = [
         gif: null,
         description: "Web app for checking Chess.com player stats and match history.",
         github: "https://github.com/JMMFL/checkm8-app",
-        topics: "React - Styled Components - API Requests"
+        topics: "React / Styled Components / API Requests / CSS Grid"
     },
 
     {
         name: "super mario cape", 
         link: "https://supermariocape.xyz/", 
         gif: null,
-        description: "Browser game inspired by Super Mario World and Flappy Bird.",
+        description: "Single-player browser game inspired by Super Mario World and Flappy Bird.",
         github: "https://github.com/JMMFL/super-mario-cape",
-        topics: "ES6+ - DOM - OOP Programming - Canvas"
+        topics: "ES6+ / DOM / OOP Programming / Canvas / Modules"
     },
 
     {
         name: "matt mazzocchi", 
         link: "https://mazzmatt.com/", 
         gif: null,
-        description: "Reponsive portfolio for Canadian photographer Matt Mazzocchi.",
+        description: "Reponsive photography portfolio for Canadian architect Matt Mazzocchi.",
         github: "https://github.com/JMMFL/photography-portfolio",
-        topics: "HTML - Flex Box - Sass - Reponsive Design"
+        topics: "Semantic HTML / CSS Flex Box / Sass / Reponsive Design"
     },
 
     {
-        name: "old galt bottle", 
+        name: "old galt bottle shop", 
         link: "https://ogbottleshop.ca/", 
         gif: null,
         description: "Shopify store for specialty beer boutique based in Cambridge.",
         github: null,
-        topics: "Liquid - CSS - Custom Cart - Custom Theme"
+        topics: "Liquid / JavaScript / CSS / Custom Cart / Custom Theme"
     }
 ]
 
