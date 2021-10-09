017a2c384156d685598ae34af1cf500b48dcb57e
JMMFL <mehravarj+github@gmail.com>
2021-10-09 16:04:55 -0400

    Display message on smaller devices.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import Works from "./components/Works";
 import { interests, profile, projects, skills } from "./data/data";
 
 function App() {
+  const deviceWidth = window.innerWidth;
+  const minWidth = 1200;
+
+  if (deviceWidth < minWidth) {
+    return (
+      <div style={{textAlign: "center"}}>
+        <h1>Mobile design coming soon.</h1>
+        <p>Please use a screen bigger than 1200 pixels wide.</p>
+      </div>
+    )
+  }
+  
   return (
     <>
       <Navigation />
